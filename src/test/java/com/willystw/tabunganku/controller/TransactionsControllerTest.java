2be765905e9e14b847af885992ab024646d7522f
Willy Setiawan <2268039+willystw@users.noreply.github.com>
2023-10-24 16:21:58 +0700

    add transaction controller

--- src/test/java/com/willystw/tabunganku/controller/TransactionsControllerTest.java
+++ src/test/java/com/willystw/tabunganku/controller/TransactionsControllerTest.java
@@ -5,12 +5,9 @@ import com.willystw.tabunganku.dto.request.AddTransactionRequest;
 import com.willystw.tabunganku.service.TransactionService;
 import com.willystw.tabunganku.service.TransactionSummaryService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,26 +16,45 @@ import java.time.LocalDate;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.lenient;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(TransactionsController.class)
 public class TransactionsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Mock
+    @MockBean
     private TransactionService transactionService;
 
-    @Mock
+    @MockBean
     private TransactionSummaryService transactionSummaryService;
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Test
+    public void createTransaction_validInput_success() throws Exception {
+        AddTransactionRequest request = new AddTransactionRequest();
+        request.setAmount(1L);
+        request.setDate(LocalDate.now());
+        request.setCategoryId(20000L);
+
+        when(transactionService.addNewTransaction(anyLong(),
+                any(LocalDate.class),
+                anyString(),
+                anyLong(),
+                anyLong())).thenReturn(
+                10L
+        );
+
+        mockMvc.perform(post("/users/123/transactions/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk());
+    }
 
     @Test
     public void createTransaction_invalidAmount_failed() throws Exception {
@@ -47,14 +63,31 @@ public class TransactionsControllerTest {
         request.setDate(LocalDate.now());
         request.setCategoryId(20000L);
 
-        //only to test if @Mock returning error or not, delete after adding more tests
-        lenient().when(transactionService.addNewTransaction(anyLong(),
-                any(LocalDate.class),
-                anyString(),
-                anyLong(),
-                anyLong())).thenReturn(
-                10L
-        );
+        mockMvc.perform(post("/users/123/transactions/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createTransaction_nullCategory_failed() throws Exception {
+        AddTransactionRequest request = new AddTransactionRequest();
+        request.setAmount(1L);
+        request.setDate(null);
+        request.setCategoryId(20000L);
+
+        mockMvc.perform(post("/users/123/transactions/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void createTransaction_nullDate_failed() throws Exception {
+        AddTransactionRequest request = new AddTransactionRequest();
+        request.setAmount(1L);
+        request.setDate(LocalDate.now());
+        request.setCategoryId(null);
 
         mockMvc.perform(post("/users/123/transactions/add")
                         .contentType(MediaType.APPLICATION_JSON)
